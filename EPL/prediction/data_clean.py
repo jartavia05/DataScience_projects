6e0fcadf47e35e737ad2f5f9f43448d499308219
Jose Artavia <jartavia@akamai.com>
2022-04-27 17:53:55 -0600

    updating the dataset file

--- EPL/prediction/data_clean.py
+++ EPL/prediction/data_clean.py
@@ -1,25 +1,26 @@
 """
 Python script to load and clean the data
 @author: jartavia05
-source: Historical results: https://www.kaggle.com/thefc17/epl-results-19932018
+source: Historical results: https://www.kaggle.com/datasets/irkaal/english-premier-league-results
 
 """
 
 
 from itertools import count
-from statistics import mean
 import pandas as pd
 
 
-history = pd.read_csv('EPL/dataset/history.csv')
+history = pd.read_csv('EPL/dataset/epl_history.csv')
 teams = pd.unique(history['HomeTeam'])
 
 # Get the history file filtered by seasons from 2010 to 2018. 
 seasons = []
-for x in list(range(10,18)): seasons.append('{}-{}'.format(2000+x,x+1))
+for x in list(range(10,22)): seasons.append('{}-{}'.format(2000+x,x+1))
 history = history.loc[history['Season'].isin(seasons)]
 
 # Calculate the average goals
 ave_home = history['FTHG'].mean()
 ave_away = history['FTAG'].mean()
-ave = mean(history['FTHG'] + history['FTAG'])
+ave = history['FTHG'].mean() + history['FTAG'].mean()
+
+print(teams)
