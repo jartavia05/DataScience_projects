0c4571b072d53a61bc725760db9e6d6bccb9bc76
Jose Artavia <jartavia@akamai.com>
2022-04-27 15:42:51 -0600

    Handle an error when no data history exist for the teams

--- EPL/prediction/match_simulate.py
+++ EPL/prediction/match_simulate.py
@@ -13,22 +13,21 @@ import pandas as pd
 
 def get_score(home, away, nsim):
     game_score = history.loc[(history['HomeTeam'] == home) & (history['AwayTeam'] == away) ]
-    ave_h_s = game_score['FTHG'].mean()
-    ave_a_s = game_score['FTAG'].mean()
+    ave_h_s = 0 if game_score['FTHG'].empty else game_score['FTHG'].mean()
+    ave_a_s = 0 if game_score['FTAG'].empty else game_score['FTAG'].mean()
 
     # Stats of the Home Team
-    t_ave_h_s = history['FTHG'].loc[history['HomeTeam'] == home].mean()
-    t_ave_h_c = history['FTAG'].loc[history['HomeTeam'] == home].mean()
+    t_ave_h_s = 0 if game_score['FTHG'].empty else game_score['FTHG'].loc[history['HomeTeam'] == home].mean()
+    t_ave_h_c = 0 if game_score['FTAG'].empty else game_score['FTAG'].loc[history['HomeTeam'] == home].mean()
 
     # Stats of the Away Team
-    t_ave_a_s = history['FTAG'].loc[history['AwayTeam'] == away].mean()
-    t_ave_a_c = history['FTHG'].loc[history['AwayTeam'] == away].mean()
+    t_ave_a_s = 0 if game_score['FTAG'].empty else game_score['FTAG'].loc[history['AwayTeam'] == away].mean()
+    t_ave_a_c = 0 if game_score['FTHG'].empty else game_score['FTHG'].loc[history['AwayTeam'] == away].mean()
   
     goals = []
     h_scored = [] 
     a_scored = []
-    for i in range(nsim):
-  
+    for i in range(nsim):  
         if len(game_score) > 3 :
             goals.append(i)
             h_scored.append(poisson.pmf(k=i,mu=ave_h_s))
@@ -42,12 +41,12 @@ def get_score(home, away, nsim):
     match_pred = {'Home': home,'Away': away,'Goals': goals, 'HomeScored': h_scored, 'AwayScored':a_scored}
     match_pred_df = pd.DataFrame(match_pred)
     print(match_pred_df)
-    match_pred_df.to_csv('match_simulate.csv')
+    match_pred_df.to_csv('match_simulate_{}_vs_{}.csv'.format(home, away))
 
     return h_scored, a_scored
         
 
 
-match_simulate = get_score('Man United','Chelsea', 6)
+match_simulate = get_score('Crystal Palace','Leeds', 6)
 print(match_simulate)
 
